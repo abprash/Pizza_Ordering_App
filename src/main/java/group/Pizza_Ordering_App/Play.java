f890a9d7d26238a4dc9e5d3bee0fc5640c5914c7
abprash <panuradh@buffalo.edu>
2017-09-13 11:03:20 -0400

    started playing. Should learn more

--- src/main/java/group/Pizza_Ordering_App/Play.java
+++ src/main/java/group/Pizza_Ordering_App/Play.java
@@ -3,7 +3,11 @@
  */
 package group.Pizza_Ordering_App;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriver.Navigation;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
@@ -16,12 +20,20 @@ public class Play {
 	public static void main(String[] args){
 		
 		System.setProperty("webdriver.gecko.driver","/home/abprashanth/resources/geckodriver");
+		System.setProperty("webdriver.chrome.driver", "/home/abprashanth/resources/chromedriver");
 		WebDriver ffDriver = new FirefoxDriver();
 		WebDriver chromeDriver = new ChromeDriver();
-		chromeDriver.get("www.google.com");
-		String content = chromeDriver.getCurrentUrl();
-		System.out.println(content);
-		chromeDriver.close();
+		ffDriver.get("https://www.google.com");
+		String content = ffDriver.getCurrentUrl();
+		String source = ffDriver.getPageSource();
+		Navigation navigator = ffDriver.navigate();
+		navigator.to("https://facebook.com");
+		Set<String> windowHandles = ffDriver.getWindowHandles();
+		//System.out.println(content);
+		//System.out.println(source);
+		System.out.println("--------------");
+		//System.out.println(windowHandles);
+		ffDriver.close();
 		
 		
 	}
