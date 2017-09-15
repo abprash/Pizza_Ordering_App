200a91cdf2b3b5dd5db555661988ec339a1f1912
abprash <panuradh@buffalo.edu>
2017-09-15 09:24:57 -0400

    lets try google searching

--- src/main/java/group/Pizza_Ordering_App/Play.java
+++ src/main/java/group/Pizza_Ordering_App/Play.java
@@ -6,8 +6,10 @@ package group.Pizza_Ordering_App;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebDriver.Navigation;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
@@ -19,22 +21,31 @@ public class Play {
 
 	public static void main(String[] args){
 		
+		//basic steps in selenium
+		//browse to a site
+		//interact with some element
+		//get results
+		//conclude with results
+		
 		System.setProperty("webdriver.gecko.driver","/home/abprashanth/resources/geckodriver");
 		System.setProperty("webdriver.chrome.driver", "/home/abprashanth/resources/chromedriver");
-		WebDriver ffDriver = new FirefoxDriver();
+		//WebDriver ffDriver = new FirefoxDriver();
 		WebDriver chromeDriver = new ChromeDriver();
-		ffDriver.get("https://www.google.com");
-		String content = ffDriver.getCurrentUrl();
-		String source = ffDriver.getPageSource();
-		Navigation navigator = ffDriver.navigate();
-		navigator.to("https://facebook.com");
-		Set<String> windowHandles = ffDriver.getWindowHandles();
-		//System.out.println(content);
-		//System.out.println(source);
-		System.out.println("--------------");
-		//System.out.println(windowHandles);
-		ffDriver.close();
+		chromeDriver.get("https://google.com");
+		//lets click a button
+		WebElement submitButton = chromeDriver.findElement(By.ById.id("_fZl"));
+		submitButton.click();
+		String content = chromeDriver.getCurrentUrl();
+		String source = chromeDriver.getPageSource();
+		Navigation navigator = chromeDriver.navigate();
+		Set<String> windowHandles = chromeDriver.getWindowHandles();
+		//navigator.to("https://facebook.com");
 		
+		//System.out.println(content);
+		System.out.println("--------------");
+		System.out.println(source);
+		//System.out.println(windowHandles);
+		chromeDriver.close();
 		
 	}
 }
